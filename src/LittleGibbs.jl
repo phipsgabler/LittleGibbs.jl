0262fd7547bff41d466ddea68f3f044c8131eacc
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-02-04 10:59:19 +0100

    Cleanup; update all blocks in one step

--- src/LittleGibbs.jl
+++ src/LittleGibbs.jl
@@ -5,48 +5,60 @@ import AbstractMCMC: step!, AbstractSampler, AbstractTransition, transition_type
 using Distributions
 using Random
 
-struct Gibbs{N, T<:NamedTuple} <: AbstractSampler
+
+struct Gibbs{B, T<:NamedTuple} <: AbstractSampler
     init_θ::T
     
-    Gibbs(init_θ::NamedTuple{N}) where {N} = new{N, NamedTuple{N}}(init_θ)
+    Gibbs(init_θ::NamedTuple{B}) where {B} = new{B, NamedTuple{B}}(init_θ)
 end
 
-struct BlockModel{N, C<:NamedTuple} <: AbstractModel
+struct BlockModel{B, C<:NamedTuple} <: AbstractModel
     conditionals::C
     
-    BlockModel(conditionals::NamedTuple{N}) where {N} = new{N, NamedTuple{N}}(conditionals)
+    BlockModel(conditionals::NamedTuple{B}) where {B} = new{B, NamedTuple{B}}(conditionals)
 end
 
-struct Transition{N, T<:NamedTuple} <: AbstractTransition
+struct Transition{B, T<:NamedTuple} <: AbstractTransition
     params::T
 
-    Transition(params::NamedTuple{N}) where {N} = new{N, NamedTuple{N}}(params)
+    Transition(params::NamedTuple{B}, block::Symbol) where {B} = new{B, NamedTuple{B}}(params)
 end
 
 
-transition_type(model::BlockModel{N, T}, spl::Gibbs{N}) where {N} = Transition{T}
+transition_type(model::BlockModel{B, T}, spl::Gibbs{B}) where {B, T} = Transition{B, T}
 
-function step!(rng::AbstractRNG, model::BlockModel{N}, spl::Gibbs{N},
-               ::Integer; kwargs...) where {N}
+function step!(rng::AbstractRNG, model::BlockModel{B}, spl::Gibbs{B},
+               ::Integer; kwargs...) where {B}
     return Transition(model.init_θ)
 end
 
-function step!(rng::AbstractRNG, model::BlockModel{N}, spl::Gibbs{N},
-               n::Integer, θ_prev::Transition{N}; kwargs...) where {N}
-    return propose(rng, spl, model, θ_prev, n)
+function step!(rng::AbstractRNG, model::BlockModel{B}, spl::Gibbs{B, T},
+               ::Integer, θ_prev::Transition{B, T}; kwargs...) where {B, T}
+    return propose(rng, spl, model, θ_prev)
 end
 
-function propose(rng::AbstractRNG, spl::Gibbs{N}, model::BlockModel{N},
-                 θ_prev::Transition{N}, n::Int)
-    block = N[mod1(n, length(N))]
-    conditional = model.conditionals[block]
-    params = rand(rng, conditional(without(θ_prev, Val(block))))
+function propose(rng::AbstractRNG, spl::Gibbs{B, T}, model::BlockModel{B},
+                 θ_prev::Transition{B, T}) where {B, T}
+    params = deepcopy(θ_prev.params)
+
+    for block in B
+        conditional = model.conditionals[block]
+        rand!(rng, params[b], conditional(without(params, Val(block))))
+    end
+    
     return Transition(params)
 end
 
-@generated function without(t::NamedTuple{N, T}, ::Val{n}) where {N, T, n}
-    parts = (:($m = t[$(QuoteNode(m))]) for m in N if m ≠ n)
+@generated function without(t::NamedTuple{B, T}, ::Val{n}) where {B, T, n}
+    parts = (:($m = t[$(QuoteNode(m))]) for m in B if m ≠ n)
     return :(;$(parts...))
+end
+
+
+function bundle_samples(rng::AbstractRNG, ::BlockModel{B, T}, ::Gibbs{B}, ::Integer,
+                        ts::Vector{Transition{B, T}}; kwargs...) where {B, T}
+    vals = copy(reduce(hcat, [values(t.params) for t in ts])')
+    return Chains(vals, B)
 end
 
 
